e149ea242e72713e192b319ac94f8f80569612c7
Max <macsj200@gmail.com>
2014-09-04 10:21:11 -0600

    Update home.client.controller.js

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -5,5 +5,12 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
 	function($scope, Authentication) {
 		// This provides Authentication context.
 		$scope.authentication = Authentication;
+		
+		$scope.myInterval = 5000;
+  		var slides = $scope.slides = [];
+  		slides.push({
+	  		image:'http://thepetcollective.tv/wp-content/uploads/2013/09/inbred-white-tiger-kenny-1.jpg',
+			text:'This is a tiger with downs syndrome'
+		});
 	}
 ]);
